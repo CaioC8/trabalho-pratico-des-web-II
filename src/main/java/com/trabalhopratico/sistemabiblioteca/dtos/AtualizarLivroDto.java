6e1949332635687e6c3dcca579cc5dc4492d22c2
CaioC8 <caiocaf@unipam.edu.br>
2026-05-08 12:54:29 -0300

    altera a forma de validar os atributos

--- src/main/java/com/trabalhopratico/sistemabiblioteca/dtos/AtualizarLivroDto.java
+++ src/main/java/com/trabalhopratico/sistemabiblioteca/dtos/AtualizarLivroDto.java
@@ -6,5 +6,5 @@ import java.util.Optional;
 
 public record AtualizarLivroDto(
     Optional<String> titulo,
-    @Size(min = 10, message = "O ISBN deve ter no mínimo 10 caractéres.") Optional<String> isbn,
-    @Positive(message = "Categoria inválida.") Optional<Long> categoriaId) {}
+    Optional<@Size(min = 10, message = "O ISBN deve ter no mínimo 10 caractéres.") String> isbn,
+    Optional<@Positive(message = "Categoria inválida.") Long> categoriaId) {}
